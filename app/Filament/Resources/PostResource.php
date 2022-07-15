6137398d21ff30e45f73f92f0b4d20c6c474208a
Ruben Baptista <inforubenbaptista@gmail.com>
2022-07-15 18:31:05 +0100

    Aplicação com Filament

---
+++ app/Filament/Resources/PostResource.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PostResource\Pages;
+use App\Filament\Resources\PostResource\RelationManagers;
+use App\Models\Post;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\TextInput;
+use Closure;
+use Illuminate\Support\Str;
+use Filament\Forms\Components\Card;
+
+use Filament\Forms\Components\Select;
+use Illuminate\Database\Eloquent\Model;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Toggle;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\BooleanColumn;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
+use Filament\Tables\Columns\SpatieMediaLibraryImageColumn;
+
+class PostResource extends Resource
+{
+    protected static ?string $model = Post::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Card::make()->schema([
+                    Select::make('category_id')
+                        ->relationship('category', 'name'),
+                    TextInput::make('title')
+                        ->reactive()
+                        ->afterStateUpdated(function (Closure $set, $state) {
+                                $set('slug', Str::slug($state));
+                            })
+                        ->required(),
+                    TextInput::make('slug')
+                        ->required(),
+                    SpatieMediaLibraryFileUpload::make('thumbnail')->collection('posts'),
+                    RichEditor::make('content'),
+                    Toggle::make('is_published')
+                ])
+                
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id')->sortable(),
+                TextColumn::make('title')->sortable(),
+                TextColumn::make('slug'),
+                BooleanColumn::make('is_published'),
+                SpatieMediaLibraryImageColumn::make('thumbnail')->collection('posts'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPosts::route('/'),
+            'create' => Pages\CreatePost::route('/create'),
+            'edit' => Pages\EditPost::route('/{record}/edit'),
+        ];
+    }    
+}
